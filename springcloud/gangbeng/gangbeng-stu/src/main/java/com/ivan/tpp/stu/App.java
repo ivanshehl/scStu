d3c76ad82bf37043bea939e967f76accc34dc001
shezhigao <shezhigao@trustrock.com.cn>
2019-04-22 17:35:45 +0800

    2019-04-22

--- springcloud/gangbeng/gangbeng-stu/src/main/java/com/ivan/tpp/stu/App.java
+++ springcloud/gangbeng/gangbeng-stu/src/main/java/com/ivan/tpp/stu/App.java
@@ -1,5 +1,10 @@
 package com.ivan.tpp.stu;
 
+import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Maps;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//        BigDecimal fenzi = BigDecimal.valueOf(0);
+//        BigDecimal mu = BigDecimal.valueOf(0);
+//        System.out.println(fenzi.divide(mu).doubleValue());
+//    	double fenzi = 10;
+//    	double mu = 0;
+//    	System.out.println(fenzi/mu);
+    	Map<String,String> map = Maps.newHashMap();
+    	map.put("code", "000000");
+    	map.put("message", "成功");
+    	map.put("contractId", "U234235435345456");
+    	map.put("phone", "Y");
+    	map.put("contact1", "Y");
+    	map.put("contact2", "Y");
+    	map.put("type", "asset");
+    	map.put("score", "60");
+    	System.out.println(JSON.toJSON(map));
+    	map = Maps.newHashMap();
+    	map.put("code", "000000");
+    	map.put("message", "成功");
+    	map.put("type", "action");
+    	map.put("score", "60");
+    	map.put("contractId", "U234235435345456");
+    	System.out.println(JSON.toJSON(map));
     }
 }
--- springcloud/gangbeng/gangbeng-stu/src/main/java/com/ivan/tpp/stu/gc/OneThread.java
+++ springcloud/gangbeng/gangbeng-stu/src/main/java/com/ivan/tpp/stu/gc/OneThread.java
@@ -1,28 +1,25 @@
 package com.ivan.tpp.stu.gc;
 
-import java.util.List;
-
-import com.google.common.collect.Lists;
 
 public class OneThread {
 	
 	public static void main(String[] args) {
 		while(true){
 			try {
-				Thread.currentThread().sleep(2000);
+				Thread.currentThread().sleep(200);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			addData(1, 10);
+			addData(10, 100);
 		}
 	}
 	
 	private static void addData(int size,int times){
-		String str = null;
+		byte [] bytes = null;
 		for(int k = 0; k < times; k++){
 			for(int i = 0; i < size; i++){
-				str = "1";
+				bytes = new byte [512];
 			}
 		}
 		
